0c7e05d305320a1b273806267adb8c1e086b3116
wkojiro1 <wkojiro@eazydrive.co.jp>
2016-04-22 12:53:28 +0000

    wakaranai

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -52,27 +52,29 @@ class RetweetsController < ApplicationController
     end
  
     def destroy
+       # retweet DELETE /retweets/:id(.:format)             retweets#destroy
  #   binding.pry    
 ##パラメータにはRetweetのIDが入っている。
   #  @retweet = current_user.retweets.find_by(id: params[:id])
 
-   @retweet = current_user.retweets.find_by(id: params[:id])
-##削除すべきレコードを@retweetにぶち込んで削除する。 
-  @micropost = Micropost.where(id: @retweet[:retweetpost_id])
 
- #  @micropost = Micropost.where(id: @retweet[:retweetpost_id])
-#    @retweet = current_user.favorites.where(id: params[:id])
-      
-    current_user.unretweet(@retweet)
-puts @micropost.inspect    
-#    flash[:success] = "retweet deleted"
-#    redirect_to request.referrer || root_url          
+
+   @retweet = current_user.retweets.find(params[:id])
+##削除すべきレコードを@retweetにぶち込んで削除する。 
+ # @micropost = Micropost.where(id: @retweet[:retweetpost_id])
+  current_user.unretweet(@retweet)
+  @micropost = Micropost.where(id: @retweet[:retweetpost_id])
+ puts @micropost.inspect
     end
     
     private
       def retweet_params
         params.require(:retweet).permit(:retweetuser_id,:retweetpost_id,:retweetcontent)
       end
- 
 
 end
+
+ #   def destroy
+ #       @user = current_user.following_relationships.find(params[:id]).followed
+ #       current_user.unfollow(@user)
+ #   end
